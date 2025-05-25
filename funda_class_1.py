474c02d1052d6ac7fc9ce24810a6e2bfcdbef0c3
DhanishReddykethireddy <dhanishreddyreddy@gmail.com>
2025-05-25 11:12:20 +0530

    Add files via upload

---
+++ funda_class_1.py
@@ -0,0 +1,59 @@
+# # i am printing my name
+# print('my name is Dhanish reddy')
+
+# '''
+# performing addition
+# by taking numbers from 
+# user
+# '''
+# number_1=int(input('enter a number:'))
+# number_2=int(input('enter a number:'))
+# result=number_1+number_2
+# print(result)
+
+# #multiple print statemnets
+# my_age=23
+# my_brother_age=27
+# print(my_age+my_brother_age)
+# print(my_age-my_brother_age)
+# print(my_age*my_brother_age)
+# print(my_age/my_brother_age)
+
+# #student marks
+# maths_marks=89 #marks gianed in maths
+# science_marks=79 #marks gained in science
+# english_marks=80 #marks gained in english
+# telugu_marks=94 #marks gained in telugu
+# social_marks=88 #marks gained in social
+# hindi_marks=99 #marks gained in hindi
+# total_marks=maths_marks+science_marks+english_marks+telugu_marks+social_marks+hindi_marks #calculating total marks
+# print(total_marks) #total marks of a student
+
+# #creation of a variables of diff types and type function
+# name='dhanish  reddy'
+# print(name)         #string
+# print(type(name))
+
+# age=24
+# print(age)             #integer
+# print(type(age))
+
+# height=5.89
+# print(height)
+# print(type(height)) #float
+
+# my_imaginary_number=4+4j
+# print(my_imaginary_number)      #compex
+# print(type(my_imaginary_number))
+
+# #id function
+
+# my_mobile_number=7674883110
+# print(id(my_mobile_number))
+
+# my_full_name='kethireddy dhanish reddy'
+# print(id(my_full_name))
+
+# my_height=5.89
+# print(id(my_height))
+
